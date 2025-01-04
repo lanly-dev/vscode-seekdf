144b6ee1810ce824910a8fe1d7a5717f51897ce8
Lan Ly <lanly-dev@users.noreply.github.com>
2025-01-03 16:55:50 -0800

    add: searchFolders()

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'
+import { ExtensionContext, RelativePattern } from 'vscode'
+import {commands, workspace } from 'vscode'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "seekdf" is now active!')
+  const rc = commands.registerCommand
+  context.subscriptions.concat([
+    rc('seekdf.searchFolders', searchFolders)
+  ])
+}
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('seekdf.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from seekdf!')
-  })
+// Function to search and return a list of target name folders
+export async function searchFolders(targetName: string): Promise<string[]> {
+  const workspaceFolders = workspace.workspaceFolders
+  if (!workspaceFolders) return []
 
-  context.subscriptions.push(disposable)
+  const result: string[] = []
+  for (const folder of workspaceFolders) {
+    const folders = await workspace.findFiles(new RelativePattern(folder, `**/${targetName}`))
+    result.push(...folders.map(f => f.fsPath))
+  }
+
+  return result
 }
 
 // This method is called when your extension is deactivated
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,7 +3,7 @@ import * as assert from 'assert'
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode'
-// import * as myExtension from '../../extension';
+import { searchFolders } from '../extension'
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.')
@@ -11,5 +11,12 @@ suite('Extension Test Suite', () => {
   test('Sample test', () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5))
     assert.strictEqual(-1, [1, 2, 3].indexOf(0))
+  })
+
+  test('searchFolders command', async () => {
+    const targetFolderName = 'targetFolderName'
+    const folders = await searchFolders(targetFolderName)
+    assert.ok(Array.isArray(folders), 'Expected folders to be an array')
+    // Add more assertions based on expected behavior
   })
 })
